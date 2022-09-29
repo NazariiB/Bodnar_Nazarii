9e9c610c478a15cd81150a68de025f38bd936e14
NazariiB <nazikbodna3@gmail.com>
2022-09-29 23:58:46 +0300

    add hw 1

---
+++ labs/src/main/java/com/epam/spring/homework1/other/OtherBeanC.java
@@ -0,0 +1,15 @@
+package com.epam.spring.homework1.other;
+
+import com.epam.spring.homework1.beans.BeanC;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OtherBeanC {
+
+    public OtherBeanC () {
+        System.out.println(beanC);
+    }
+    @Autowired
+    public BeanC beanC;
+}
